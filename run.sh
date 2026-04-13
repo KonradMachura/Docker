3946fd4b0a331a1f78a3055d4da45d81b6314ef8
Konrad <konrad.machura@idego.io>
2026-04-13 13:32:31 +0200

    Changing project sturcture

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 # Build the image once
-docker build -t hello-container .
+# Build context is the hello/ subfolder — Docker looks for Dockerfile there
+docker build -t hello-container hello/
 
 # Run 5 containers, each with a different number
 # for i in 1 2 3 4 5; do
